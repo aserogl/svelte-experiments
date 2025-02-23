e7d0d77b5780b2df542b6bc6a4a479131cb2317d
aserogl <aserogl31@gmail.com>
2025-02-23 12:15:59 +0500

    feat(svelte config): enable scss preprocessing

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import { mdsvex } from 'mdsvex'
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex()],
+  preprocess: [vitePreprocess(), mdsvex(), sveltePreprocess()],
 
   kit: {
     adapter: adapter(),
