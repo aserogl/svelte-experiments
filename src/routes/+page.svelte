a1d4f2d6526d2e46fb85c8a5f1c3a352e46533e8
aserogl <aserogl31@gmail.com>
2025-02-23 12:16:29 +0500

    feat: scss example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="parent">
+  <div class="child">Child 1</div>
+  <div class="child">Child 2</div>
+</div>
+
+<style lang="scss">
+  @mixin clearfix() {
+    &:before,
+    &:after {
+      content: '';
+      display: table;
+    }
+    &:after {
+      clear: both;
+    }
+  }
+  .parent {
+    @include clearfix();
+
+    .child {
+      float: left;
+      width: 50%;
+      box-sizing: border-box;
+      padding: 10px;
+      background-color: #f0f0f0;
+      border: 1px solid #ccc;
+    }
+  }
+</style>
